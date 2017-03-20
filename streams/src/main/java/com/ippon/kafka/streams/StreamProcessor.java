f817c2563015e363b2ead5fb8479e4db7892d177
Garcia Florian <garcia.floriian@gmail.com>
2017-03-20 20:30:14 +0100

    Fix NPE + clean for demo :v:

--- streams/src/main/java/com/ippon/kafka/streams/StreamProcessor.java
+++ streams/src/main/java/com/ippon/kafka/streams/StreamProcessor.java
@@ -32,18 +32,26 @@ public class StreamProcessor implements CommandLineRunner {
         // Create an instance of StreamsConfig from the Properties instance
         StreamsConfig config = new StreamsConfig(getProperties());
 
+
+        // ------------------------------------------------------
+        //       Define Serialization / Deserialization
+        // ------------------------------------------------------
         Map<String, Object> serdeProps = new HashMap<>();
-        // Create Effectif Serde
         Serde<Effectif> effectifSerde = SerdeFactory.createSerde(Effectif.class, serdeProps);
 
-        // building Kafka Streams Model
+        // Building Kafka Streams Model
         KStreamBuilder kStreamBuilder = new KStreamBuilder();
+
+        // Init common topology
         KStream<Integer, Effectif> effectifsStream = kStreamBuilder.stream(Serdes.Integer(), effectifSerde, inputTopic)
                 .filter((year, effectif) -> !"TOTAL".equals(effectif.getGroup()))
                 .filter((year, effectif) -> "Commune".equals(effectif.getGeographicLevel()));
 
-        // ------------------------------------
-        // 1. Global student count per year
+        // ------------------------------------------------------
+        //              Define streams topologies               |
+        // ------------------------------------------------------
+        //          1. Global student count per year            |
+        // ------------------------------------------------------
         KTable<Integer, Double> studentsPerYear = effectifsStream
                 .map((year, effectif) -> new KeyValue<>(year, effectif.getStudentCount()))
                 .groupByKey(Serdes.Integer(), Serdes.Double())
@@ -55,10 +63,22 @@ public class StreamProcessor implements CommandLineRunner {
 
         // We can easily send to another topic
         studentsPerYear.to(Serdes.Integer(), Serdes.Double(), "studentsPerYear");
+        // Or print the result
         studentsPerYear.print();
 
-        // ------------------------------------
-        // 2. Students per commune 2014
+        // ------------------------------------------------------
+        //              2. Formation communes count             |
+        // ------------------------------------------------------
+        effectifsStream
+                .groupBy((year, effectif) -> effectif.getGroup(), Serdes.String(), effectifSerde)
+                .count("countPerFormation")
+                .print();
+
+
+        // ------------------------------------------------------
+        //              3. Student count variation
+        // ------------------------------------------------------
+        // Students per commune 2014
         KTable<String, Double> studentsPerCommune2014 = effectifsStream
                 .filter((year, effectif) -> year == 2014)
                 .groupBy((year, effectif) -> effectif.getGeographicUnit(), Serdes.String(), effectifSerde)
@@ -69,8 +89,7 @@ public class StreamProcessor implements CommandLineRunner {
                         Serdes.Double(),
                         "studentsPerCommune2014");
 
-        // ------------------------------------
-        // 3. Students per commune 2015
+        // Students per commune 2015
         KTable<String, Double> studentsPerCommune2015 = effectifsStream
                 .filter((year, effectif) -> year == 2015)
                 .groupBy((year, effectif) -> effectif.getGeographicUnit(), Serdes.String(), effectifSerde)
@@ -81,23 +100,32 @@ public class StreamProcessor implements CommandLineRunner {
                         Serdes.Double(),
                         "studentsPerCommune2015");
 
-        // ------------------------------------
-        // 4. Student count variation per commune
+        // Student count variation between 2014 and 2015
         studentsPerCommune2014
-                .leftJoin(studentsPerCommune2015, (value2014, value2015) -> (1 - value2014 / value2015) * 100)
+                .leftJoin(studentsPerCommune2015, this::calculateEvolution)
                 .print();
 
-        // ------------------------------------
-        // 5. Formation communes count
-        effectifsStream
-                .groupBy((year, effectif) -> effectif.getGroup(), Serdes.String(), effectifSerde)
-                .count("countPerFormation")
-                .print();
-
+        // ------------------------------------------------------
+        //                  Start processing
+        // ------------------------------------------------------
         System.out.println("Starting Kafka Streams");
         KafkaStreams kafkaStreams = new KafkaStreams(kStreamBuilder, config);
         kafkaStreams.start();
         System.out.println("Kafka Streams started");
+    }
+
+    private Double calculateEvolution(Double previousValue, Double newValue) {
+        if (previousValue == null && newValue == null) {
+            return 0D;
+        }
+        if (previousValue == null) {
+            return 100D;
+        }
+        if (newValue == null) {
+            return -100D;
+        }
+
+        return  (newValue / previousValue) * 100;
     }
 
     /**
@@ -106,14 +134,15 @@ public class StreamProcessor implements CommandLineRunner {
      */
     private static Properties getProperties() {
         Properties settings = new Properties();
-        // Set a few key parameters
+        // Application ID, used for consumer groups
         settings.put(StreamsConfig.APPLICATION_ID_CONFIG, STREAM_APPLICATION_NAME);
         // Kafka bootstrap server (broker to talk to)
         settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         // default serdes for serialzing and deserializing key and value from and to streams
         settings.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         settings.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        // reset to earliest
+
+        // We can also set Consumer properties
         settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return settings;
     }
