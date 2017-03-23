74af2f7cdc8e9a31cae78ed9dba94ab0ad08608c
Garcia Florian <garcia.floriian@gmail.com>
2017-03-23 13:45:48 +0100

    :kiss:

--- basic-consumer/src/main/java/com/ippon/kafka/basic/BasicConsumeApp.java
+++ basic-consumer/src/main/java/com/ippon/kafka/basic/BasicConsumeApp.java
@@ -1,12 +1,54 @@
 package com.ippon.kafka.basic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ippon.kafka.playground.model.Effectif;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Properties;
+
 /**
  * Created by @ImFlog on 15/02/2017.
  */
 public class BasicConsumeApp {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final Logger logger = LoggerFactory.getLogger(BasicConsumeApp.class);
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
 
-        new Thread(new BasicConsumer()).start();
+    private static Long count = 0L;
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "basic");
+        // props.put("enable.auto.commit", "true");
+        // props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singleton("effectifs"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                try {
+                    Effectif effectif = jsonMapper.readValue(record.value(), Effectif.class);
+                    count++;
+                    if (count % 10000 == 0) {
+                        logger.info("Read {} messages, last => year : {}, location : {}",
+                                count,
+                                effectif.getYear(),
+                                effectif.getGeographicUnit());
+                    }
+                } catch (IOException e) {
+                    logger.error("Could not deserialize message");
+                }
+            }
+        }
     }
 }
--- basic-producer/src/main/java/com/ippon/kafka/basic/BasicProduceApp.java
+++ basic-producer/src/main/java/com/ippon/kafka/basic/BasicProduceApp.java
@@ -1,14 +1,92 @@
 package com.ippon.kafka.basic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ippon.kafka.playground.model.Effectif;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Properties;
+
 /**
  * Created by @ImFlog on 15/02/2017.
  */
 public class BasicProduceApp {
 
-    public static void main(String[] args) throws InterruptedException {
-        // Set producer input file from command line args
-        BasicProducer.INPUT_FILE = args[0];
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(BasicProduceApp.class);
 
-        new Thread(new BasicProducer()).start();
+    private static Long count = 0L;
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        // props.put("acks", "1");
+        // props.put("retries", 1);
+        // props.put("compression.type", "none"); // Could be gzip, snappy or lz4
+        //props.put("batch.size", 16384);
+        // props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Init producer from props
+        KafkaProducer<Integer, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // Read file
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(BasicProduceApp.class.getResourceAsStream("/" + args[0])));
+        bufferedReader.lines()
+                .skip(1L) // Skip headers
+                .map(BasicProduceApp::readCsv)
+                .filter(Objects::nonNull)
+                .forEach(effectif -> sendToKafka(kafkaProducer, effectif));
+        logger.info("Finished reading {} elements", count);
+    }
+
+    private static void sendToKafka(KafkaProducer<Integer, String> kafkaProducer, Effectif effectif) {
+        try {
+            kafkaProducer.send(
+                    new ProducerRecord<>(
+                            "effectifs",
+                            effectif.getYear(), // Shard by year
+                            jsonMapper.writeValueAsString(effectif)));
+            count++;
+            if (count % 10000 == 0) {
+                logger.info("Sent {} messages, last => year : {}, location : {}",
+                        count, effectif.getYear(), effectif.getGeographicUnit());
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private static Effectif readCsv(String line) {
+        String[] columns = line.split(";");
+        return new Effectif(
+                Integer.parseInt(columns[0]),
+                columns[1],
+                columns[2],
+                columns[3],
+                columns[4],
+                columns[5],
+                columns[6],
+                columns[7],
+                columns[8],
+                columns[9],
+                columns[10].isEmpty() ? null : Double.parseDouble(columns[10]),
+                columns[11],
+                columns[12].isEmpty() ? null : Double.parseDouble(columns[12]),
+                columns[13],
+                columns[14].isEmpty() ? null : Double.parseDouble(columns[14]),
+                columns[15],
+                columns[16],
+                columns[17],
+                columns[18],
+                columns[19],
+                columns[20]);
     }
 }
